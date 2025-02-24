0e1f6ef678762cf0e0dc89ae52e8501d08843a36
Ritesh <ritesh.sargam00@gmail.com>
2025-02-24 23:36:09 +0530

    Adding Swagger

--- eCommerce.API/Program.cs
+++ eCommerce.API/Program.cs
@@ -24,6 +24,23 @@ builder.Services.AddAutoMapper(typeof
 //FluenValidations
 builder.Services.AddFluentValidationAutoValidation();
 
+//Add API explorer services
+builder.Services.AddEndpointsApiExplorer();
+
+//Add swagger generation services to create swagger specification
+builder.Services.AddSwaggerGen();
+
+//Add cors services
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins("http://localhost:4200")
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
+});
+
 //Build the web application
 var app = builder.Build();
 
@@ -31,6 +48,9 @@ app.UseExceptionHandlingMiddleware();
 
 //Routing
 app.UseRouting();
+app.UseSwagger(); //Adds endpoint that can server the swagger.json
+app.UseSwaggerUI(); // Adds swagger UI (interactive page to explore and test API endpoints)
+app.UseCors();
 
 //Auth
 app.UseAuthentication();
